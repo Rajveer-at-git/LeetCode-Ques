d0e7728f6d4055ee26892c0c21ff76aff443961c
Rajveer <levelingup1923@gmail.com>
2025-12-18 14:10:11 +0530

    Unordered Set

--- 1 DSA Notes/Set/Unordered_set.cpp
+++ 1 DSA Notes/Set/Unordered_set.cpp
@@ -1,5 +1,13 @@
 // Time complexity: O(1) usually (Unordered set and map are super optimized data structures) 
 // Stored items will be in random order
+// fns like 
+// insert(x) : inserts element x  
+// find(x) : returns the iterator to the element if the elementis present
+// count(x) : return true if present else false
+// erase(x) : returns 1 if x is present and then deletes it, if x is absent returns 0
+// clear() : empties set
+// are allowed
+ 
 #include <iostream>
 #include <vector>
 #include <string>
@@ -25,4 +33,14 @@ int main() {
         cout << val << " ";
     }
     cout << endl;
+
+    cout << *(s.find(4)) << endl; // 4
+    cout << s.count(4) << endl; // true or 1
+    cout << s.erase(4) << endl;
+    for(auto val : s)
+    {
+        cout << val << " ";
+    }
+    cout << endl;
 }
+
