4321175a4544831fd614755c16983b51baf98a5b
Rajveer <levelingup1923@gmail.com>
2026-01-03 22:34:20 +0530

    Day 2

--- Substring Matching Pattern/Solution.cpp
+++ Substring Matching Pattern/Solution.cpp
@@ -5,8 +5,8 @@
 using namespace std;
 
 int main() {
-    string s = "Rajveer";
-    string p = "aj*er";
+    string s = "ckckkk";
+    string p = "ck*kc";
 
     int star = p.find('*');
         string prestar = p.substr(0,star);
@@ -14,14 +14,27 @@ int main() {
         if(prestar != "")
         {
             if(s.find(prestar) != string::npos);
-            else return(false);
+            else {cout << false; return 0;}
         }
-        
+
         if(poststar != "" && p.find(poststar) > p.find(prestar))
         {
             if(s.find(poststar) != string::npos);
-            else return(false);
+            else 
+            {
+                cout << false;
+                return 0;
+            }
+        }
+        cout << prestar << endl << poststar << endl;
+        if(prestar != "" && poststar != "" && prestar.find(poststar) != string::npos)
+        {
+            int new_p = s.size() - poststar.length();
+            if(s.find(poststar,new_p) != string::npos);
+            else {cout << false; return 0;}
+            return 0;
         }
 
-        return true;
+
+        cout << true;
 }
