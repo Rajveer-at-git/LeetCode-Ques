fbe3b9f53a0f37caa2ffb3a4af5894dc014878be
Rajveer <levelingup1923@gmail.com>
2026-01-07 13:36:17 +0530

    Progress

--- Substring Matching Pattern/Solution.cpp
+++ Substring Matching Pattern/Solution.cpp
@@ -5,36 +5,51 @@
 using namespace std;
 
 int main() {
-    string s = "ckckkk";
-    string p = "ck*kc";
+    string s = "tokk";
+    string p = "t*t";  
 
     int star = p.find('*');
-        string prestar = p.substr(0,star);
-        string poststar = p.substr(star+1,p.size()-1);
-        if(prestar != "")
-        {
-            if(s.find(prestar) != string::npos);
-            else {cout << false; return 0;}
-        }
+    string prestar = p.substr(0,star);
+    string poststar = p.substr(star+1,p.size()-1);
+    string non_star = p.erase(star,1);
+    cout << prestar << endl << poststar << endl << non_star << endl;
 
-        if(poststar != "" && p.find(poststar) > p.find(prestar))
+    if(s.find(non_star) != string::npos)
+    {
+        cout << true;
+        return 0;
+    }
+
+    if(prestar != "")
+    {
+        if(s.find(prestar) != string::npos);
+        else {cout << false; return 0;}
+    }
+
+    if(poststar != "" && p.find(poststar) > p.find(prestar))
+    {
+        if(s.find(poststar) != string::npos);
+        else 
         {
-            if(s.find(poststar) != string::npos);
-            else 
-            {
-                cout << false;
-                return 0;
-            }
-        }
-        cout << prestar << endl << poststar << endl;
-        if(prestar != "" && poststar != "" && prestar.find(poststar) != string::npos)
-        {
-            int new_p = s.size() - poststar.length();
-            if(s.find(poststar,new_p) != string::npos);
-            else {cout << false; return 0;}
+            cout << false;
             return 0;
         }
+    }
 
+    if(prestar.find(poststar))
+    {
+        int new_p = s.find(prestar)+1;
+        if(s.find(poststar,new_p) != string::npos);
+        else {cout << false; return 0;}
+    }
 
-        cout << true;
+    if(prestar != "" && poststar != "")
+    {
+        int new_p = s.size() - poststar.length();
+        if(s.find(poststar,new_p) != string::npos);
+        else {cout << false; return 0;}
+        return 0;
+    }
+
+    cout << true;
 }
