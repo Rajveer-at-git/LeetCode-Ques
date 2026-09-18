527883b8450f065f2e5b24acdc50c622681f6e15
Rajveer Choudhary <levelingup1923@gmail.com>
2026-09-18 15:00:11 +0530

    Brute approach

---
+++ 1_DSA_Notes/Topic Based Problems/Recursion/6_Palindrome/Brute_force.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int left = 0, right = s.length()-1;
+
+    while(left<right)
+    {
+        if(!isalnum(s[left]))
+            left++;
+        else if(!isalnum(s[right]))
+            right--;
+        else if( tolower(s[left]) != tolower(s[right]))
+            return false;
+        else
+        {
+            left++;
+            right--;
+        }
+    }
+    return true;
+}
